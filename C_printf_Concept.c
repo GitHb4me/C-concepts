c3675e1fb0ea09fa57f8f351e97271bf7e48efab
Sam <92459398+GitHb4me@users.noreply.github.com>
2022-08-26 10:56:01 +0530

    Update C_printf_Concept.c

--- C_printf_Concept.c
+++ C_printf_Concept.c
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
+  int x, y;
+  scanf("%d%d", &x, &y); //place holder or format specifier
+  return x+y;
+  printf("%d%d",x+y);  // printf directly performs operation without need of assigning one more for storing the result
+  printf("%0.4f,%0.2f",3.88657+5.33356); // printf also performs approximation round off if value after threshold is >=5 ++1 else no increment
+}
     int a;long b;char c;float d ;double e; 
      cin>>a>>b>>c>>d>>e;
      cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<fixed<<setprecision(3)<<d<<"\n"<<fixed<<setprecision(9)<<e;// Complete the code.
