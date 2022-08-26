46c8cab05dd260c32682fc120212cb874e7df81c
Sam <92459398+GitHb4me@users.noreply.github.com>
2022-08-26 10:16:56 +0530

    Create Break&Return_Concept.cpp

---
+++ Break&Return_Concept.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stdio.h>
+int main(){
+    int s=7;
+    for(;s>0;s--)
+    {
+        if(s== 4){  
+            break;
+        }std::cout<<s<<"\t";
+    }
+    for(;s>0;s--){
+        std::cout<<s<<std::endl;
+    }
+    }  
