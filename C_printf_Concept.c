dd356de0a004d22a7b62cff8886de43539f46f69
Sam <92459398+GitHb4me@users.noreply.github.com>
2022-08-26 10:51:00 +0530

    Update and rename CprintfConcept.c to C_printf_Concept.c

---
+++ C_printf_Concept.c
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;long b;char c;float d ;double e; 
+     cin>>a>>b>>c>>d>>e;
+     cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<fixed<<setprecision(3)<<d<<"\n"<<fixed<<setprecision(9)<<e;// Complete the code.
+    return 0;
+}
