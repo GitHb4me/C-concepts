d23c83fd8783e41b8de03825145fa7fb8d639baa
mckinsey <92459398+mckinsy@users.noreply.github.com>
2022-08-30 14:10:13 +0530

    Update conditionals.cpp

--- conditionals.cpp
+++ conditionals.cpp
@@ -1,1 +1,47 @@
+                     // In Flow Charts this concept is visualized pictorially under  " Decision Box " 
+int Type1()
+{
+  if()
+  if()
+  if()
+  if()
+    }
+int Type2()
+{
+  if() 
+    else
+  if()                  // don't confuse it is similar to else if though written in new line both means same
+    else
+  if()
+    else
+    //OR
+  if()
+  else if()
+  else if()
+  else// here this else will work like as  " default: "    in    " switch "   case, if no statement is true directly else executes
+    }
+loops nested()
+{
+  if()
+  {
+    if()
+    {
+      if()
+      else            // this can continue till .....n times   
+        }
+  }
+  else
+  {
+    if() 
+    { 
+      if() 
+        else { 
+          if()
+          else
+              }            // this can continue till ....n times
+        }
+    else{
+    }
+  }
+  // no bracket { } requires if only single statement is after the loop or conditionals but if more than one statement present then it should be indented within braces  " { } "
 
