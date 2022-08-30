72d62d71a2ba1bf7fe7d8616101c77776330cf34
mckinsey <92459398+mckinsy@users.noreply.github.com>
2022-08-30 13:38:59 +0530

    Update loops.cpp

--- loops.cpp
+++ loops.cpp
@@ -1,1 +1,40 @@
-
+int loops1()
+{
+  if()
+  if()
+  if()
+  if()
+    }
+int lops2()
+{
+  if() 
+    else
+  if()                  // don't confuse it is similar to else if though written in new line both eans same
+    else
+  if()
+    else// here this else will work like as  " default: "    in    " switch "   case, if no statement is true directly else executes
+    }
+loops nested()
+{
+  if()
+  {
+    if()
+    {
+      if()
+      else            // this can continue till .....n times   
+        }
+  }
+  else
+  {
+    if() 
+    { 
+      if() 
+        else { 
+          if()
+          else
+              }            // this can continue till ....n times
+        }
+    else{
+    }
+  }
+  // no bracket { } requires if only single statement is after the loop or conditionals but if more than one statement present then it should be indented within braces  " { } "
