46451dc0193d939a2e96a14e9c257291e1d490ab
mckinsey <92459398+mckinsy@users.noreply.github.com>
2022-08-30 13:59:22 +0530

    Create TernaryOperator.cpp

---
+++ TernaryOperator.cpp
@@ -0,0 +1,8 @@
+same as if else
+  
+  if(statement 1 & statement 2 | statement 3)
+  Block a;
+  else
+  Block b;
+               // in ternary terms  ? : ;
+    (statemen 1 & statement 2 | statement 3) ? Block a : Block b;   // braces {} does not support inside ternary operator means only one  " block "  execute at once
