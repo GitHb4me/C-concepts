2596b4c2cf792d7351f92646c16534778d4b5d7c
mckinsey <92459398+mckinsy@users.noreply.github.com>
2022-09-03 09:59:27 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using std::cout;
+int main() {
+    int x=5;
+    int s=x++ + ++x;
+std::cout<<s;
+return 0; //pre (increase then use) and post (use then increase) increment operator concept
+}
