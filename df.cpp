958965a12706c2f156373e9af5c21f5242abc015
mckinsey <92459398+mckinsy@users.noreply.github.com>
2022-09-03 10:05:03 +0530

    Add files via upload

---
+++ df.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<iostream> 
+using std::cout;
+using std::cin;
+using std::endl;
+int main(){
+    int n;int count=1; 
+    cin>>n;
+    for(int i=1;i<=n*2-1;i++){
+    for(int j=i;j<=n*2-1;j++){count++;
+    cout<<j;}cout<<endl;}
+}  //work on your aura and self confidence to impress any one in first sight ,personality
