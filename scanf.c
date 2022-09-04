46efea8bc65e655c86a1e498d85876cada5c4238
mckinsey <92459398+mckinsy@users.noreply.github.com>
2022-09-04 20:01:42 +0530

    Update scanf.c

--- scanf.c
+++ scanf.c
@@ -4,5 +4,5 @@ int main()
     char c[12];
     scanf("%[^\n]",&c);  // scanf don't stores whitespace as soon as it encounters whitespace scanf terminates so this format specifier placeholder helps to scan whitespaces in between 
     printf("output: %s",c);
-   printf("%d%d",4*4); ----> it can directly perform operations
+   printf("%d%d",4*4);// ----> printf function can also perform operations
 }
