5bf0ca6059ffb150303fbea02d5953a0f05d86ae
mckinsey <92459398+mckinsy@users.noreply.github.com>
2022-09-04 11:37:33 +0530

    Update Printf.c

--- Printf.c
+++ Printf.c
@@ -5,7 +5,7 @@ int main() {
   int x, y;
   scanf("%d%d", &x, &y); //place holder or format specifier
   return x+y;
-  printf("%d%d",x+y);  // printf directly performs operation without need of assigning one more for storing the result
+  printf("%d%d",x+y);  // printf directly performs operation without need of assigning one more variable for storing the result
   printf("%0.4f,%0.2f",3.88657+5.33356); // printf also performs approximation round off if value after threshold is >=5 ++1 else no increment
 }
     int a;long b;char c;float d ;double e; 
