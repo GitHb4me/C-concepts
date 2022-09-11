9f55cc3b093d76b082c9f19b3136c0c8016c01c7
mckinsey <92459398+mckinsy@users.noreply.github.com>
2022-09-11 17:15:10 +0530

    Create speedUp.cpp

---
+++ speedUp.cpp
@@ -0,0 +1,14 @@
+// incorporate this statement will speeds up c++ program
+// for more info visit link - https://stackoverflow.com/questions/48367983/why-does-the-following-snippet-speed-up-the-code
+// https://www.geeksforgeeks.org/fast-io-for-competitive-programming/
+// https://www.geeksforgeeks.org/speed-up-naive-algorithms-in-competitive-coding-in-c-cpp/
+// https://www.geeksforgeeks.org/basic-code-optimizations-in-c/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
