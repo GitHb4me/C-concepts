42390446f9cbce0467d3a93a6429d2d180caf2bf
Tarun <92459398+pokemondodge@users.noreply.github.com>
2024-03-30 22:47:35 +0530

    Update macros.cpp

--- macros.cpp
+++ macros.cpp
@@ -1,1 +1,32 @@
 
+#include<stdio.h>
+#include<stdbool.h>
+#include<conio.h>
+#include<stdlib.h>
+    #define scan(v) scanf("%d",&v)
+    #define print "hello"
+    #define AREA(l, b) (l * b)
+ 
+int main2()
+{
+ 
+
+    int l1 = 10, l2 = 5, area;
+ 
+    area = AREA(l1, l2);
+ 
+    printf("Area of rectangle is: %d", area);
+   
+    return 0;
+}
+   
+int main(void)
+{
+
+    func(67);
+    main2();
+    printf("\n %d %d",scan(v),123);
+    getch();
+    printf()
+    return 0;
+}
